6e7607eec1426184504e27e481e82c73342fe3e4
nicomedrano <nicomedrano@users.noreply.github.com>
2023-12-26 16:33:47 -0600

    Update script

--- data-dictionary-generator.py
+++ data-dictionary-generator.py
@@ -10,10 +10,49 @@ Functions:
     main: The main function of the application.
 """
 
+import json
 import streamlit as st
+import boto3
 from toolkit.env import EnvironmentVariableManager
 from toolkit.googlesheets import open_google_sheets_client
 from toolkit.snowflakedb import SnowflakeDB
+
+AWS_ACCESS_ID = EnvironmentVariableManager().getenv("AWS_ACCESS_ID")
+AWS_ACCESS_KEY = EnvironmentVariableManager().getenv("AWS_ACCESS_KEY")
+AWS_REGION = EnvironmentVariableManager().getenv("SECRETS_AWS_REGION")
+
+SNOWSQL_ACCOUNT = None
+SNOWSQL_USER = None
+SNOWSQL_PWD = None
+SNOWSQL_WAREHOUSE = None
+SNOWSQL_DATA_LAKE = None
+GC_SERVICE_ACCOUNT_CREDENTIALS = None
+
+
+def update_secrets(secret_manager) -> None:
+    """update necessary credentials from AWS Secrets Manager.
+
+    Parameters:
+        secret_manager: AWS Secrets Manager object
+    """
+    secret_value = secret_manager.get_secret_value(
+        SecretId="/polco_ardem_ingestion/env"
+    )
+    secret_string = secret_value["SecretString"]
+    secret = json.loads(secret_string)
+
+    global SNOWSQL_ACCOUNT
+    SNOWSQL_ACCOUNT = secret["SNOWSQL_ACCOUNT"]
+    global SNOWSQL_USER
+    SNOWSQL_USER = secret["SNOWSQL_USER"]
+    global SNOWSQL_PWD
+    SNOWSQL_PWD = secret["SNOWSQL_PWD"]
+    global SNOWSQL_WAREHOUSE
+    SNOWSQL_WAREHOUSE = secret["SNOWSQL_WAREHOUSE"]
+    global GC_SERVICE_ACCOUNT_CREDENTIALS
+    GC_SERVICE_ACCOUNT_CREDENTIALS = json.loads(
+        secret["GC_SERVICE_ACCOUNT_CREDENTIALS"]
+    )
 
 
 def save_survey_data_to_google_sheets(survey_data, spreadsheet_url):
@@ -27,11 +66,9 @@ def save_survey_data_to_google_sheets(survey_data, spreadsheet_url):
         None
     """
     gsheets_service_account = EnvironmentVariableManager().getenv(
-        "GSHEETS_SERVICE_ACCOUNT"
+        "GC_SERVICE_ACCOUNT_CREDENTIALS"
     )
-    # Get the GSheets client using the toolkit
     client = open_google_sheets_client(gsheets_service_account)
-    # Recycle the Service Account credentials object
     sh = client.open_by_url(spreadsheet_url)
     worksheet = sh.worksheet("Data Dictionary")
     survey_data.fillna(0, inplace=True)
@@ -45,6 +82,11 @@ def main():
     """
     Main function to run the Streamlit application.
     """
+    awsSession = boto3.Session(
+        aws_access_key_id=AWS_ACCESS_ID, aws_secret_access_key=AWS_ACCESS_KEY
+    )
+    secret = awsSession.client("secretsmanager", region_name=AWS_REGION)
+    update_secrets(secret)
     st.title("Data Dictionary Generator")
     conn = SnowflakeDB()
     survey_id = st.text_input("Enter the Survey ID")
